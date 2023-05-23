0b46838dfe839c7e9b590bca87b667c2b8703946
Artmal23 <118556570+Artmal23@users.noreply.github.com>
2023-05-23 17:08:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ async function logJSONData() {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=7b4f5cceb1e65adb013e08bc2f1149c0");
     const jsonData = await response.json();
     console.log(jsonData)
+    console.log(jsonData.main.temp)
+    console.log(jsonData.main.humidity)
+    console.log(jsonData.wind.speed)
     but.addEventListener("click", cl)
     function cl(){    
         temp.innerHTML=`<span>${jsonData.main.temp} градусів</span>`
