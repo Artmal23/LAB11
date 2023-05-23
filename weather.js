c14ed6c1abda3bab2fbd903a734e894894af5e3d
Artmal23 <118556570+Artmal23@users.noreply.github.com>
2023-05-23 17:03:11 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,2 +1,16 @@
- const response = fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=7b4f5cceb1e65adb013e08bc2f1149c0");
- response.json();
+temp = document.querySelector('.temp')
+hum = document.querySelector('.hum')
+wind = document.querySelector('.wind')
+but = document.querySelector('.but')
+async function logJSONData() {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=7b4f5cceb1e65adb013e08bc2f1149c0");
+    const jsonData = await response.json();
+    console.log(jsonData)
+    but.addEventListener("click", cl)
+    function cl(){    
+        temp.innerHTML=`<span>${jsonData.main.temp} градусів</span>`
+        hum.innerHTML=`<span>${jsonData.main.humidity} %</span>`
+        wind.innerHTML=`<span>${jsonData.wind.speed} м/с</span>`
+        }
+  }
+  logJSONData()
